d6aa18e126ab61734f88aefbabe133396a8ce8b3
Vadym Polietaiev <va2dim@gmail.com>
2018-03-12 21:59:44 +0200

    Modified Autoload;

--- autoload.php
+++ autoload.php
@@ -1,16 +1,6 @@
 <?php
 
-/**
- * TODO __autoload
- * @return false если файл класса не найден - use если несколько ф-ций __autoload, тогда spl_autoload_register() принимает эти ф-ции и + в очередь.
- *
- * Пример преобразования
- * App\Models\User => ./App/Models/User.php
- */
-function __autoload($class_name)
-{
-    require __DIR__ . '/' . str_replace('\\', '/', $class_name) . '.php';
+spl_autoload_register(function ($className) {
+    require __DIR__ . '/' . str_replace('\\', '/', $className) . '.php';
+});
 
-    // SPL composer сам делает
-    //include __DIR__ . '/vendor/autoload.php';
-}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
 //require __DIR__ . '/boot.php';
 require __DIR__ . '/autoload.php';
 
